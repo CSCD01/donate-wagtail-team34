2dcd086a49bdf3e86709d54d3eba360def0834fa
caleb1111 <calebchen1997@gmail.com>
2020-03-11 12:37:19 -0400

    completed the tests for one-time and monthly, documentation added

--- donate/payments/tests/test_monthly.py
+++ donate/payments/tests/test_monthly.py
@@ -2,20 +2,33 @@ from selenium import webdriver
 import time
 # driver = webdriver.FireFox(driver_path)
 url = "http://0.0.0.0:8000/"
-driver_path = "/Users/xinzheng/Mozilla/donate-wagtail-team34/donate/payments/tests/driver/chromedriver"
+driver_path = "./driver/chromedriver"
 driver = webdriver.Chrome(driver_path)
 driver.set_page_load_timeout(10)
 driver.get(url)
+# click on the monthly radio button
 radio_select = driver.find_element_by_id("tab-2")
 radio_select.click()
 time.sleep(2)
+# select the second option in the monthly tab-panel and click on it
 amount_select = driver.find_element_by_xpath('//label[@for="monthly-amount-1"]')
 amount_select.click()
 time.sleep(2)
+# select the visa payment button and click on it
 visa_select = driver.find_element_by_xpath('//*[@id="donate-form--monthly"]/div[2]/div/button')
 visa_select.click()
 time.sleep(2)
-driver.execute_script("window.history.go(-1)");
+# perform the go back action
+driver.execute_script("window.history.go(-1)")
 time.sleep(2)
+# select the second option in the monthly tab-panel
+expected_option = driver.find_element_by_xpath('//label[@for="monthly-amount-1"]')
+try:
+    # only the dispayed/visible elements are clickable
+    # perform the click => success!
+    expected_option.click()
+    print("Test completed successfully!")
+except:
+    # if click was not successfully performed => the option is not displayed
+    print("Monthly options are not displayed! Test failed!")
 driver.close()
-print("Test completed successfully!")
--- donate/payments/tests/test_one_time.py
+++ donate/payments/tests/test_one_time.py
@@ -2,20 +2,33 @@ from selenium import webdriver
 import time
 # driver = webdriver.FireFox(driver_path)
 url = "http://0.0.0.0:8000/"
-driver_path = "/Users/xinzheng/Mozilla/donate-wagtail-team34/donate/payments/tests/driver/chromedriver"
+driver_path = "./driver/chromedriver"
 driver = webdriver.Chrome(driver_path)
 driver.set_page_load_timeout(10)
 driver.get(url)
+# click on the one-time radio button
 radio_select = driver.find_element_by_id("tab-1")
 radio_select.click()
 time.sleep(2)
+# select the second option in the one-time tab-panel and click on it
 amount_select = driver.find_element_by_xpath('//label[@for="one-time-amount-1"]')
 amount_select.click()
 time.sleep(2)
+# select the visa payment button and click on it
 visa_select = driver.find_element_by_class_name("button__icons")
 visa_select.click()
 time.sleep(2)
-driver.execute_script("window.history.go(-1)");
+# perform the go back action
+driver.execute_script("window.history.go(-1)")
 time.sleep(2)
+# select the second option in the one-time tab-panel
+expected_option = driver.find_element_by_xpath('//label[@for="one-time-amount-1"]')
+try:
+    # only the dispayed/visible elements are clickable
+    # perform the click => success!
+    expected_option.click()
+    print("Test completed successfully!")
+except:
+    # if click was not successfully performed => the option is not displayed
+    print("One-time options do not exist! Test failed!")
 driver.close()
-print("Test completed successfully!")
