5c87c2c2dde88c2b11c680bd4cc59d578d615f06
Xin Zheng <williamzhxin@gmail.com>
2020-04-07 14:10:24 -0400

    fix mistakes and add a chromedriver

--- donate/payments/tests/test_no_value_monthly_fr.py
+++ donate/payments/tests/test_no_value_monthly_fr.py
@@ -16,11 +16,10 @@ time.sleep(2)
 visa_select = driver.find_element_by_xpath('//*[@id="donate-form--monthly"]/div[2]/div/button')
 visa_select.click()
 time.sleep(2)
-expect_option = driver.find_element_by_id("payments__braintree-errors-paypal")
 try:
-    if(expect_option.size() != 0):
-        print("Test SUCCESS!\n");
+    driver.find_element_by_id("payments__braintree-errors-paypal")
+    print("Test SUCCESS!\n")
 except:
     # if no proper error display then test fail
-    print("No error message shows on Monthly panel. Test Failed!\n");
+    print("No error message shows on Monthly panel. Test Failed!\n")
 driver.close()
--- donate/payments/tests/test_no_value_visa_monthly.py
+++ donate/payments/tests/test_no_value_visa_monthly.py
@@ -3,7 +3,7 @@ import time
 
 url = "http://0.0.0.0:8000/"
 # user can choose webdriver of chrome or firefox
-driver_path = './driver/chromedriver'
+driver_path = "./driver/chromedriver"
 driver = webdriver.Chrome(driver_path)
 driver.set_page_load_timeout(10)
 driver.get(url)
@@ -16,11 +16,10 @@ time.sleep(2)
 visa_select = driver.find_element_by_xpath('//*[@id="donate-form--monthly"]/div[2]/div/button')
 visa_select.click()
 time.sleep(2)
-expect_option = driver.find_element_by_id("payments__braintree-errors-paypal")
 try:
-    if(expect_option.size() != 0):
-        print("Test SUCCESS!\n");
+    driver.find_element_by_id("payments__braintree-errors-paypal")
+    print("Test SUCCESS!\n");
 except:
     # if no proper error display then test fail
-    print("No error message shows on Monthly panel. Test Failed!\n");
+    print("No error message shows on Monthly panel. Test Failed!\n")
 driver.close()
