9e90bc653b39f8c9528a25c8dd3a745b3a04baed
Xin Zheng <williamzhxin@gmail.com>
2020-04-07 18:38:02 -0400

    complete acceptance test

--- donate/payments/tests/test_no_value_monthly_fr.py
+++ donate/payments/tests/test_no_value_monthly_fr.py
@@ -16,10 +16,10 @@ time.sleep(2)
 visa_select = driver.find_element_by_xpath('//*[@id="donate-form--monthly"]/div[2]/div/button')
 visa_select.click()
 time.sleep(2)
-try:
-    driver.find_element_by_id("payments__braintree-errors-paypal")
-    print("Test SUCCESS!\n")
-except:
+expect = driver.find_element_by_id("payments__braintree-errors-paypal")
+if(expect.text == "No amount selected! Please select a donation amount!"):
+    print("Test SUCCESS!\n");
+else:
     # if no proper error display then test fail
-    print("No error message shows on Monthly panel. Test Failed!\n")
+    print("No error message shows on Monthly panel. Test Failed!\n");
 driver.close()
--- donate/payments/tests/test_no_value_visa_onetime.py
+++ donate/payments/tests/test_no_value_visa_onetime.py
@@ -13,13 +13,13 @@ radio_select = driver.find_element_by_id("tab-1")
 radio_select.click()
 time.sleep(2)
 # select the visa payment button and click on it
-visa_select = visa_select = driver.find_element_by_class_name("button__icons")
+visa_select = driver.find_element_by_class_name("button__icons")
 visa_select.click()
 time.sleep(2)
-try:
-    driver.find_element_by_id("payments__braintree-errors-paypal")
-    print("Test SUCCESS!\n")
-except:
+expect = driver.find_element_by_id("payments__braintree-errors-paypal")
+if(expect.text == "No amount selected! Please select a donation amount!"):
+    print("Test SUCCESS!\n");
+else:
     # if no proper error display then test fail
-    print("No error message shows on Monthly panel. Test Failed!\n")
+    print("No error message shows on Monthly panel. Test Failed!\n");
 driver.close()
